a06cd8ff02cbc31e7b15599e7110b73057f5ae3b
Gromov Roman <romzes.rz@gmail.com>
2023-08-06 19:02:36 +0300

    first commit

---
+++ src/BMI.java
@@ -0,0 +1,9 @@
+public class BMI {
+    public float calculate (float weight,float height) {
+        float result;
+        float height2;
+        height2 = height * height;
+        result = weight / height2;
+        return (int) result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.lang.module.FindException;
+
+public class Main {
+    public static void main(String[] args) {
+        BMI service = new BMI();
+        float weight = 98;
+        float height = 1.87F;
+        int BMI = (int) service.calculate(weight,height);
+        System.out.println((BMI) + " Индекс тела");
+    }
+}
